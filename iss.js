8713e8bac39a23433d780ccda4a78d3c16521026
KishReboot <russellsalopek@pm.me>
2022-11-24 19:43:04 -0500

    Added being able to error for general reasons, or error for a server reason.

--- iss.js
+++ iss.js
@@ -1,21 +1,15 @@
-/**
- * Makes a single API request to retrieve the user's IP address.
- * Input:
- *   - A callback (to pass back an error or the IP string)
- * Returns (via Callback):
- *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
- *  
- * 
- */
-const request = require('request'); 
+const request = require('request');
 
 const myIP = `https://api.ipify.org?format=json`;
 
-const fetchMyIP = (callback) => { 
+const fetchMyIP = (callback) => {
   
-  request (myIP, (error, response, body) => {
+  request(myIP, (error, response, body) => {
 
+    if (error) callback(`Request failed: ${error}`, null);
+
+    if (response.statusCode !== 200) return callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
+    
     const ip = JSON.parse(body).ip;
     
     callback(null, ip);
